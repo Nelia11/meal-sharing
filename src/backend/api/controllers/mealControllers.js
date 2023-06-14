c19e060f5ce0eb16d270eaaa20f71e2614d9bbb4
Nelia11 <levit.nelia@icloud.com>
2023-06-14 10:16:50 +0200

    refactor meal endpoints and add validation for enhanced functionality

---
+++ src/backend/api/controllers/mealControllers.js
@@ -0,0 +1,105 @@
+const knex = require("../../database");
+
+const getAll = async (req, res) => {
+    try {
+      const meals = await knex("meal").select("id", "title");
+  
+      meals.length === 0
+      ? res.status(404).json({"error": "Meal not found"})
+      : res.status(200).json(meals);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+};
+
+const addMeal = async (req, res) => {
+    try {
+      const meal = req.body;
+      const id = meal.id;
+  
+      const mealExists = await knex("meal").where({id}).select(1).first();
+  
+      const requiredFields = ["id", "title", "description", "location", "when", "max_reservations", "price", "created_date"];
+      const missingMandatoryField = requiredFields.some((field)=> !req.body[field]);
+  
+      if (mealExists) {
+        res.status(400).json({"error": "Meal already exsists."});
+        return;
+      } else if (missingMandatoryField) {
+        res.status(400).json({"error": "Incomplite meal data"});
+        return;
+      } else if (isNaN(meal.price) || meal.price <= 0) {
+        res.status(400).json({"error": "Invalid price"});
+        return;
+      } else {
+        await knex("meal").insert(meal);
+        res.status(201).json({"message": "Added meal"});
+      }
+    } catch (error) {
+      res.status(500).json(error);
+    }
+};
+
+const getById = async (req, res) => {
+    try {
+      const id = req.params.id;
+  
+      const meal = await knex("meal").select("id", "title").where({id});
+  
+      meal.length === 0
+      ? res.status(404).json({"error": "Meal not found"})
+      : res.status(200).json(meal);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+};
+
+const updateById =  async (req, res) => {
+    try {
+      const id = req.params.id;
+      const meal = req.body;
+  
+      const mealExists = await knex("meal").where({id}).select(1).first();
+  
+      if (!mealExists) {
+        res.status(404).json({"error": "Meal not found"});
+        return;
+      } else if (mealExists) {
+  
+        if (Object.keys(meal).length === 0) {
+          res.status(400).json({"error": "Please, enter a new meal information."});
+          return;
+        } else {
+        await knex("meal").where({id}).update(meal);
+        res.status(200).json({"message": "Updated meal"});
+        }
+        
+      }
+    } catch (error) {
+      res.status(500).json(error);
+    }
+};
+
+const deleteById = async (req, res) => {
+    try {
+      const id = req.params.id;
+  
+      const mealExists = await knex("meal").where({id}).select(1).first();
+  
+      await knex("meal").where({id}).del();
+  
+      !mealExists
+      ? res.status(404).json({"error": "Meal not found"})
+      : res.status(200).json({"message": "Deleted meal"});
+    } catch (error) {
+      res.status(500).json(error);
+    }
+};
+
+module.exports = {
+    getAll,
+    addMeal,
+    getById,
+    updateById,
+    deleteById
+}
--- src/backend/api/meals.js
+++ src/backend/api/meals.js
@@ -1,78 +1,20 @@
 const express = require("express");
-const router = express.Router();
-const knex = require("../database");
+const mealRouter = express.Router();
+const { getAll, addMeal, getById, updateById, deleteById } = require("./controllers/mealControllers");
 
 // http://localhost:5000/api/meals	GET,	Returns all meals
-router.get("/", async (req, res) => {
-  try {
-    const meals = await knex("meal").select("id", "title");
-
-    meals.length === 0
-    ? res.status(404).send("Meals not found")
-    : res.status(200).send(meals);
-  } catch (error) {
-    res.status(500).json(error);
-  }
-});
+mealRouter.get("/", getAll);
 
 // /api/meals	POST, Adds a new meal to the database
-router.post("/", async (req, res) => {
-  try {
-    const meal = req.body;
-
-    const addMeal = await knex("meal").insert(meal);
-    res.status(201).send({"message": "Added meal"});
-  } catch (error) {
-    res.status(500).json(error);
-  }
-});
+mealRouter.post("/", addMeal);
 
 // /api/meals/:id	GET,	Returns the meal by id
-router.get("/:id", async (req, res) => {
-  try {
-    const id = Number(req.params.id);
-
-    const meal = await knex("meal").select("id", "title").where({id});
-
-    meal.length === 0
-    ? res.status(404).send("Meal not found")
-    : res.status(200).send(meal);
-  } catch (error) {
-    res.status(500).json(error);
-  }
-});
+mealRouter.get("/:id", getById);
 
 // /api/meals/:id PUT,	Updates the meal by id
-router.put("/:id", async (req, res) => {
-  try {
-    const id = Number(req.params.id);
-    const meal = req.body;
-
-    const meals = await knex("meal").select("id");
-    const updateMeal = await knex("meal").where({id}).update(meal);
-    
-    id > meals.length
-    ? res.status(404).send("Meal not found")
-    : res.status(200).send({"message": "Updated meal"});
-  } catch (error) {
-    res.status(500).json(error);
-  }
-});
+mealRouter.put("/:id", updateById);
 
 // /api/meals/:id	DELETE,	Deletes the meal by id
-router.delete("/:id", async (req, res) => {
-  try {
-    const id = Number(req.params.id);
+mealRouter.delete("/:id", deleteById);
 
-    const meals = await knex("meal").select("id");
-    const deleteMeal = await knex("meal").where({id}).del();
-
-    id > meals.length
-    ? res.status(404).send("Meal not found")
-    : res.status(200).send({"message": "Deleted meal"});
-  } catch (error) {
-    res.status(500).json(error);
-  }
-});
-
-module.exports = router;
+module.exports = mealRouter;
